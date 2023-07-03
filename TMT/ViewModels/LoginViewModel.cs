3bd55450bc0f09d95924b358ce6bc3c7524e2eb5
Renas Shekho <105039988+RenasShekho@users.noreply.github.com>
2023-07-03 11:26:44 +0200

    user note input( user control - text box and button).

--- TMT/ViewModels/LoginViewModel.cs
+++ TMT/ViewModels/LoginViewModel.cs
@@ -9,13 +9,13 @@ namespace TMT.ViewModels
 {
     internal class LoginViewModel:ViewModelBase
     {
-        private string _name = "HelLo";
+        //private string _name = "HelLo";
 
-        public string Name
-        {
-            get => _name;
-            set => this.RaiseAndSetIfChanged(ref _name, value);
-        }
+        //public string Name
+        //{
+        //    get => _name;
+        //    set => this.RaiseAndSetIfChanged(ref _name, value);
+        //}
 
 
         
---
+++ TMT/Views/UserNoteInput.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TMT.Views
+{
+    public partial class UserNoteInput : UserControl
+    {
+        public UserNoteInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
